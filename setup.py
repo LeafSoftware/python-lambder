1026a693f151026bed067ecd1e638469a1f33fbc
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-01-22 14:54:52 -0500

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dependencies = [
 
 setup(
     name='lambder',
-    version='0.1.0',
+    version='1.0.0',
     url='https://github.com/LeafSoftware/python-lambder',
     license='MIT',
     author='Chris Chalfant',
